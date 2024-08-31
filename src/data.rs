b30b49b23a6b3cea737a3e1477963701424c4016
telephono <telephono@users.noreply.github.com>
2024-08-31 10:07:50 +0200

    We don't need the aircraft's ICAO code

--- src/data.rs
+++ src/data.rs
@@ -4,8 +4,6 @@ use xplm::data::borrowed::{DataRef, FindError};
 use xplm::data::{DataRead, ReadOnly, ReadWrite, StringRead};
 
 pub struct BorrowedDataRefs {
-    // ICAO code for aircraft (a string) entered by author
-    pub acf_icao: DataRef<[u8], ReadOnly>,
     // Path of current livery. Ends in dir separator. WARNING: slow dataref, don't read a lot!
     pub acf_livery_path: DataRef<[u8], ReadOnly>,
     // Fuel Tank 1 Weight (kg)
@@ -19,10 +17,6 @@ impl Debug for BorrowedDataRefs {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("BorrowedDataRefs")
             .field(
-                "acf_icao",
-                &self.acf_icao.get_as_string().unwrap_or_default(),
-            )
-            .field(
                 "acf_livery_path",
                 &self.acf_livery_path.get_as_string().unwrap_or_default(),
             )
@@ -35,7 +29,6 @@ impl Debug for BorrowedDataRefs {
 impl BorrowedDataRefs {
     pub fn initialize() -> Result<Self, FindError> {
         let datarefs = BorrowedDataRefs {
-            acf_icao: DataRef::find("sim/aircraft/view/acf_ICAO")?,
             acf_livery_path: DataRef::find("sim/aircraft/view/acf_livery_path")?,
             m_fuel1: DataRef::find("sim/flightmodel/weight/m_fuel1")?.writeable()?,
             m_fuel2: DataRef::find("sim/flightmodel/weight/m_fuel2")?.writeable()?,
